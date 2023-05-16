7155a6357a89c7ae564a313237f92080f8e9308c
kamakozi <86746560+kamakozi@users.noreply.github.com>
2023-05-16 16:01:05 +0200

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,6 @@
+name = input("Please enter your name: ")
+if name == str(name):
+    print(str(name))
+else:
+    jls_extract_var = "Only letters1"
+    print(jls_extract_var)
---
+++ name = "Edis".py
@@ -0,0 +1,2 @@
+name = "Edis"
+print(name)
